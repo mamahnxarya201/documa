cc76c20bd2da2d61e83840666966e4e6923cc611
Aryana Diaz <diazcakasana201@gmail.com>
2024-11-11 16:11:02 +0700

    add basic modal

--- resources/js/Pages/Document.vue
+++ resources/js/Pages/Document.vue
@@ -3,16 +3,23 @@
 import AppLayout from "@/Layouts/AppLayout.vue";
 import {ChevronDownIcon, ArrowTopRightOnSquareIcon, ChevronUpIcon} from "@heroicons/vue/24/solid/index.js";
 import {ref} from "vue";
+import ConfirmationModal from "@/Components/ConfirmationModal.vue";
+import DangerButton from "@/Components/DangerButton.vue";
+import SecondaryButton from "@/Components/SecondaryButton.vue";
+import DialogModal from "@/Components/DialogModal.vue";
 
-const openDropdown = ref(false)
 const items = ref([{id: 1, name: 'test', openDropdown: false}, {id: 2, name: 'test', openDropdown: false}, {id: 3, name: 'test', openDropdown: false}, {id: 4, name: 'test', openDropdown: false}])
+const editModal = ref(false)
+const cancelEdit = ref(false)
+
 const trStyling = (idx) => (
     items.value[idx].openDropdown
     ? "text-center bg-gray-100 border-b dark:bg-gray-800 dark:border-gray-700 dark:text-white"
     : "text-center bg-white border-b dark:bg-gray-800 dark:border-gray-700 dark:text-white")
 
-const dropDown = (idx) => items.value[idx].openDropdown = !items.value[idx].openDropdown;
-
+const dropDown = (idx) => items.value[idx].openDropdown = !items.value[idx].openDropdown
+const openEditModal = () => editModal.value = true
+const cancelEditModal = () => cancelEdit.value = true
 </script>
 
 <template>
@@ -33,7 +40,7 @@ const dropDown = (idx) => items.value[idx].openDropdown = !items.value[idx].open
                                                 <th>Action</th>
                                                 <th>Detail</th>
                                             </thead>
-                                            <tbody >
+                                            <tbody>
                                                 <template v-for="(item, key) in items" :key="key">
                                                     <tr :class="trStyling(key)">
                                                         <td>test gfdsgsdsl</td>
@@ -41,16 +48,16 @@ const dropDown = (idx) => items.value[idx].openDropdown = !items.value[idx].open
                                                         <td>test</td>
                                                         <td>
                                                             <div class="flex w-full justify-center">
-                                                                <a href="/document" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">
+                                                                <div class="font-medium text-blue-600 dark:text-blue-500 hover:underline" @click="openEditModal">
                                                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                                                         <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"/>
                                                                     </svg>
-                                                                </a>
-                                                                <a href="/document" class="font-medium text-blue-600 dark:text-blue-500 hover:text-gray-400">
+                                                                </div>
+                                                                <div class="font-medium text-blue-600 dark:text-blue-500 hover:text-gray-400">
                                                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                                                         <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"/>
                                                                     </svg>
-                                                                </a>
+                                                                </div>
                                                             </div>
                                                         </td>
                                                         <td>
@@ -80,7 +87,7 @@ const dropDown = (idx) => items.value[idx].openDropdown = !items.value[idx].open
                                                                         <div class="text-xl text-gray-400">Total Files</div>
                                                                         <div class="flex flex-row">
                                                                             <div class="text-base">10</div>
-                                                                            <ArrowTopRightOnSquareIcon class="size-4 ml-2 text-blue-600 dark:text-blue-500 hover:underline" @click="console.log('galo')"/>
+                                                                            <ArrowTopRightOnSquareIcon class="size-4 ml-2 text-blue-600 dark:text-blue-500 hover:underline" @click="openEditModal"/>
                                                                         </div>
                                                                     </div>
                                                                     <div class="flex flex-col">
@@ -122,6 +129,24 @@ const dropDown = (idx) => items.value[idx].openDropdown = !items.value[idx].open
                 </div>
             </div>
         </div>
+
+        <DialogModal :show="editModal" @close="editModal = false">
+            <template #title>
+                API Token
+            </template>
+
+            <template #content>
+                <div>
+                    Please copy your new API token. For your security, it won't be shown again.
+                </div>
+            </template>
+
+            <template #footer>
+                <SecondaryButton @click="editModal = false">
+                    Close
+                </SecondaryButton>
+            </template>
+        </DialogModal>
     </AppLayout>
 </template>
 

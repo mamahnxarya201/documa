20fb9ad4dc344f30831be9fc5dbec6bdf237330d
Aryana Diaz <diazcakasana201@gmail.com>
2024-11-12 15:08:38 +0700

    add database migration model seeder factory

---
+++ database/migrations/2024_11_12_073557_create_documents_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('documents', function (Blueprint $table) {
+            $table->uuid();
+            $table->string('name');
+            $table->foreign('author')->references('id')->on('users');
+            $table->jsonb('shared_to');
+            $table->jsonb('notes');
+            $table->jsonb('attachment');
+            $table->jsonb('comment');
+            $table->jsonb('tags');
+            $table->foreign('reviewer')->references('id')->on('users');
+            $table->foreign('status')->references('id')->on('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('documents');
+    }
+};

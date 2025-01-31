6b94c76e9f329464549934a8edb47464a16ca321
Aryana Diaz <diazcakasana201@gmail.com>
2025-01-31 14:16:49 +0700

    add some reactivity to form

--- assets/controllers/create_document_form_controller.js
+++ assets/controllers/create_document_form_controller.js
@@ -1,23 +1,20 @@
 // assets/controllers/create_document_form_controller.js
-import { Controller } from '@hotwired/stimulus';
+import {Controller} from '@hotwired/stimulus';
 
 export default class extends Controller {
     static targets = ['groupShareWrapper', 'userShareWrapper', 'externalShareWrapper', 'usernameWrapper'];
 
-    toggleShareOptions(event) {
-        const selectedOption = event.target.value;
-        this.groupShareWrapperTarget.classList.add('d-none');
-        this.userShareWrapperTarget.classList.add('d-none');
-        this.externalShareWrapperTarget.classList.add('d-none');
-        this.usernameWrapperTarget.classList.add('d-none');
+    toggleShareOptions({ target: { value: selectedOption, checked } }) {
+        const shareTargets = {
+            'group': [this.groupShareWrapperTarget],
+            'user': [this.userShareWrapperTarget, this.usernameWrapperTarget],
+            'external_link': [this.externalShareWrapperTarget]
+        };
 
-        if (selectedOption === 'group') {
-            this.groupShareWrapperTarget.classList.remove('d-none');
-        } else if (selectedOption === 'user') {
-            this.userShareWrapperTarget.classList.remove('d-none');
-            this.usernameWrapperTarget.classList.remove('d-none');
-        } else if (selectedOption === 'external_link') {
-            this.externalShareWrapperTarget.classList.remove('d-none');
-        }
+        const toggleMethod = checked ? 'remove' : 'add';
+
+        shareTargets[selectedOption]?.forEach(target =>
+            target.classList[toggleMethod]('d-none')
+        );
     }
 }
